fc54983b5ade1025d1f8adb793c00aaec77ecc22
Ik Sung Choi <ikchoi0@gmail.com>
2022-05-11 15:19:28 -0400

    move commands

--- client.js
+++ client.js
@@ -11,7 +11,6 @@ const connect = function () {
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
   });
-  conn.write("Name: LOL");
   return conn;
 };
 
--- play.js
+++ play.js
@@ -1,7 +1,20 @@
 const { connect } = require("./client");
 
+var stdin = process.stdin;
+
 console.log("Connecting ...");
 const conn = connect();
 conn.on("data", (data) => {
   console.log(data);
 });
+conn.write("Name: LOL");
+
+conn.write("Move: up");
+setTimeout(() => {
+      conn.write("Move: up");
+}, 100);
+// setInterval(() => {
+//   console.log('moving up');
+//   conn.write("Move: up");
+// }, 50);
+
